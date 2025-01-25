bc1ea3f73ebdcf545c2a7eff5c4b1d916fd86564
Bayhaqiindra <127074133+Bayhaqiindra@users.noreply.github.com>
2025-01-25 11:27:18 +0700

    Menambahkan ViewModel untuk memasukkan data acara, dengan fungsi penyimpanan acara dan penanganan error

--- app/src/main/java/com/example/bismillahtestiprojectucp/ui/viewmodel/acara/InsertAcaraViewModel.kt
+++ app/src/main/java/com/example/bismillahtestiprojectucp/ui/viewmodel/acara/InsertAcaraViewModel.kt
@@ -1,2 +1,73 @@
-package com.example.bismillahtestiprojectucp.ui.viewmodel.acara
+package com.example.bismillahtestiprojectucp.viewmodel
 
+import android.util.Log
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.bismillahtestiprojectucp.model.Acara
+import com.example.bismillahtestiprojectucp.repository.AcaraRepository
+import kotlinx.coroutines.launch
+
+class InsertAcaraViewModel(private val acr: AcaraRepository) : ViewModel() {
+    var uiacaraState by mutableStateOf(InsertAcaraUiState())
+        private set
+    fun updateInsertAcaraState(insertUiEvent: InsertAcaraUiEvent) {
+        uiacaraState = InsertAcaraUiState(insertUiEvent = insertUiEvent)
+    }
+
+    fun insertAcara() {
+        viewModelScope.launch {
+            try {
+                acr.insertAcara(uiacaraState.insertUiEvent.toAcara())
+            } catch (e: Exception) {
+                e.printStackTrace()
+                Log.e("InsertAcaraViewModel", "Insert failed", e)
+            }
+        }
+    }
+}
+
+data class InsertAcaraUiState(
+    val insertUiEvent: InsertAcaraUiEvent = InsertAcaraUiEvent(),
+    val isLoading: Boolean = false,
+    val isSuccess: Boolean = false
+)
+
+data class InsertAcaraUiEvent(
+    val idAcara: String = "",
+    val namaAcara: String = "",
+    val deskripsiAcara: String = "",
+    val tanggalMulai: String = "",
+    val tanggalBerakhir: String = "",
+    val idLokasi: String = "",
+    val idKlien: String = "",
+    val statusAcara: String = ""
+)
+
+fun InsertAcaraUiEvent.toAcara(): Acara = Acara(
+    idAcara = idAcara,
+    namaAcara = namaAcara,
+    deskripsiAcara = deskripsiAcara,
+    tanggalMulai = tanggalMulai,
+    tanggalBerakhir = tanggalBerakhir,
+    idLokasi = idLokasi,
+    idKlien = idKlien,
+    statusAcara = statusAcara
+)
+
+fun Acara.toUiStateAcara(): InsertAcaraUiState = InsertAcaraUiState(
+    insertUiEvent = toInsertUiEvent()
+)
+
+fun Acara.toInsertUiEvent(): InsertAcaraUiEvent = InsertAcaraUiEvent(
+    idAcara = idAcara,
+    namaAcara = namaAcara,
+    deskripsiAcara = deskripsiAcara,
+    tanggalMulai = tanggalMulai,
+    tanggalBerakhir = tanggalBerakhir,
+    idLokasi = idLokasi,
+    idKlien = idKlien,
+    statusAcara = statusAcara
+)
