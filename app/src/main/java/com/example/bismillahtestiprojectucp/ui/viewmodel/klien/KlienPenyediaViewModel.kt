c7429032e4baa17c38cf060f7c7d4bf666dcf31c
Bayhaqiindra <127074133+Bayhaqiindra@users.noreply.github.com>
2025-01-25 11:23:17 +0700

    Menambahkan factory untuk ViewModel klien dan menghubungkan ke repository dengan aplikasi

--- app/src/main/java/com/example/bismillahtestiprojectucp/ui/viewmodel/klien/KlienPenyediaViewModel.kt
+++ app/src/main/java/com/example/bismillahtestiprojectucp/ui/viewmodel/klien/KlienPenyediaViewModel.kt
@@ -1,2 +1,23 @@
 package com.example.bismillahtestiprojectucp.ui.viewmodel.klien
 
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.createSavedStateHandle
+import androidx.lifecycle.viewmodel.CreationExtras
+import androidx.lifecycle.viewmodel.initializer
+import androidx.lifecycle.viewmodel.viewModelFactory
+import com.example.bismillahtestiprojectucp.EventApplications
+import com.example.bismillahtestiprojectucp.ui.viewmodel.acara.DetailAcaraViewModel
+import com.example.bismillahtestiprojectucp.ui.viewmodel.acara.UpdateAcaraViewModel
+import com.example.bismillahtestiprojectucp.ui.viewmodel.acara.acaraApp
+
+object KlienPenyediaViewModel {
+    val Factory = viewModelFactory {
+        initializer { HomeKlienViewModel(acaraApp().container.klienRepository) }
+        initializer { InsertKlienViewModel(acaraApp().container.klienRepository) }
+        initializer { DetailKlienViewModel(createSavedStateHandle(), klen = acaraApp().container.klienRepository) }
+        initializer { UpdateKlienViewModel(createSavedStateHandle(), klen = acaraApp().container.klienRepository) }
+    }
+}
+
+fun CreationExtras.acaraApp(): EventApplications =
+    (this[ViewModelProvider.AndroidViewModelFactory.APPLICATION_KEY] as EventApplications)
