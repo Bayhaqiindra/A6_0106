70d3812eccf7fae57bad4064481e079d91983f93
Bayhaqiindra <127074133+Bayhaqiindra@users.noreply.github.com>
2025-01-25 11:30:13 +0700

    Menambahkan ViewModel untuk mengelola status klien, termasuk memuat, menangani error, dan berhasil mendapatkan data klien dari repository.

--- app/src/main/java/com/example/bismillahtestiprojectucp/ui/viewmodel/klien/HomeKlienViewModel.kt
+++ app/src/main/java/com/example/bismillahtestiprojectucp/ui/viewmodel/klien/HomeKlienViewModel.kt
@@ -1,2 +1,41 @@
 package com.example.bismillahtestiprojectucp.ui.viewmodel.klien
 
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.bismillahtestiprojectucp.model.Klien
+import com.example.bismillahtestiprojectucp.repository.KlienRepository
+import kotlinx.coroutines.launch
+import retrofit2.HttpException
+import java.io.IOException
+
+sealed class KlienUiState {
+    data class Success(val klien: List<Klien>) : KlienUiState()
+    object Error : KlienUiState()
+    object Loading : KlienUiState()
+}
+
+class HomeKlienViewModel(private val klen: KlienRepository) : ViewModel() {
+
+    var klienUiState: KlienUiState by mutableStateOf(KlienUiState.Loading)
+        private set
+
+    init {
+        getKlien()
+    }
+
+    fun getKlien() {
+        viewModelScope.launch {
+            klienUiState = KlienUiState.Loading
+            klienUiState = try {
+                KlienUiState.Success(klen.getKlien())
+            } catch (e: IOException) {
+                KlienUiState.Error
+            } catch (e: HttpException) {
+                KlienUiState.Error
+            }
+        }
+    }
+}
