286f1540e17923c387be1bee9c87c32c2fe162a8
Bayhaqiindra <127074133+Bayhaqiindra@users.noreply.github.com>
2025-01-25 11:34:43 +0700

    Menambahkan inisialisasi ViewModel untuk DetailAcara dan UpdateAcara dengan menggunakan SavedStateHandle dan AcaraRepository

--- app/src/main/java/com/example/bismillahtestiprojectucp/ui/viewmodel/acara/AcaraPenyediaViewModel.kt
+++ app/src/main/java/com/example/bismillahtestiprojectucp/ui/viewmodel/acara/AcaraPenyediaViewModel.kt
@@ -1,6 +1,7 @@
 package com.example.bismillahtestiprojectucp.ui.viewmodel.acara
 
 import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.createSavedStateHandle
 import androidx.lifecycle.viewmodel.CreationExtras
 import androidx.lifecycle.viewmodel.initializer
 import androidx.lifecycle.viewmodel.viewModelFactory
@@ -11,6 +12,8 @@ object AcaraPenyediaViewModel {
     val Factory = viewModelFactory {
         initializer { HomeAcaraViewModel(acaraApp().container.acaraRepository) }
         initializer { InsertAcaraViewModel(acaraApp().container.acaraRepository) }
+        initializer { DetailAcaraViewModel(createSavedStateHandle(), acr = acaraApp().container.acaraRepository) }
+        initializer { UpdateAcaraViewModel(createSavedStateHandle(), acr = acaraApp().container.acaraRepository) }
     }
 }
 
