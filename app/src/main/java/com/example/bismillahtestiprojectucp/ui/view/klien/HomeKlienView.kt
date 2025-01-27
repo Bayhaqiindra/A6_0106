cced36dc85ec760c5e2c2415d97ada79726524a6
Bayhaqiindra <127074133+Bayhaqiindra@users.noreply.github.com>
2025-01-28 04:47:01 +0700

    Memperbaiki code Tampilan

--- app/src/main/java/com/example/bismillahtestiprojectucp/ui/view/klien/HomeKlienView.kt
+++ app/src/main/java/com/example/bismillahtestiprojectucp/ui/view/klien/HomeKlienView.kt
@@ -1,5 +1,7 @@
 package com.example.bismillahtestiprojectucp.ui.view.klien
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
@@ -7,34 +9,48 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
-import androidx.compose.material.icons.filled.Delete
+import androidx.compose.material.icons.filled.ArrowForward
+import androidx.compose.material.icons.filled.Call
+import androidx.compose.material.icons.filled.Menu
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.Divider
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
-import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
-import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.input.nestedscroll.nestedScroll
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.shadow
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.bismillahtestiprojectucp.R
 import com.example.bismillahtestiprojectucp.model.Klien
 import com.example.bismillahtestiprojectucp.navigation.DestinasiNavigasi
-import com.example.bismillahtestiprojectucp.ui.customwidget.CostumeTopAppBar
 import com.example.bismillahtestiprojectucp.ui.view.acara.OnError
 import com.example.bismillahtestiprojectucp.ui.view.acara.OnLoading
 import com.example.bismillahtestiprojectucp.ui.viewmodel.klien.HomeKlienViewModel
@@ -54,41 +70,73 @@ fun KlienHomeScreen(
     onDetailClick: (String) -> Unit = {},
     viewModel: HomeKlienViewModel = viewModel(factory = KlienPenyediaViewModel.Factory)
 ) {
-    val scrollBehavior = TopAppBarDefaults.enterAlwaysScrollBehavior()
 
     LaunchedEffect(Unit) {
         viewModel.getKlien()
     }
 
-    Scaffold(
-        modifier = modifier
-            .nestedScroll(scrollBehavior.nestedScrollConnection),
-        topBar = {
-            CostumeTopAppBar(
-                title = DestinasiKlienHome.titleRes,
-                canNavigateBack = false,
-                scrollBehavior = scrollBehavior,
-                onRefresh = {
-                    viewModel.getKlien()
-                }
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(color = colorResource(R.color.primary)),
+    ) {
+
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(start = 16.dp),
+            horizontalArrangement = Arrangement.SpaceBetween,
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Text(
+                text = "Daftar Klien",
+                fontFamily = FontFamily.Serif,
+                fontWeight = FontWeight.Bold,
+                style = TextStyle(
+                    fontSize = 35.sp
+                ),
+                color = Color.White,
             )
-        },
-        floatingActionButton = {
+            Image(
+                painter = painterResource(id = R.drawable.logo),
+                contentDescription = "Logo",
+                modifier = Modifier
+                    .size(110.dp),
+            )
+        }
+
+        Box(
+            modifier = Modifier
+                .fillMaxSize()
+                .clip(RoundedCornerShape(topStart = 30.dp, topEnd = 30.dp)) // Membentuk lengkungan
+                .background(color = colorResource(R.color.yellow))
+                .offset(y = -10.dp)
+                .padding(top = 8.dp, bottom = 40.dp)
+        ) {
+            Column(modifier = Modifier) {
+                KlienStatus(
+                    klienUiState = viewModel.klienUiState,
+                    retryAction = { viewModel.getKlien() },
+                    modifier = Modifier,
+                    onDetailClick = onDetailClick,
+                )
+            }
+
             FloatingActionButton(
                 onClick = navigateToItemEntry,
-                shape = MaterialTheme.shapes.medium,
-                modifier = Modifier.padding(18.dp)
+                shape = RoundedCornerShape(20.dp), // Bentuk bulat
+                containerColor = colorResource(R.color.primary), // Warna primary
+                modifier = Modifier
+                    .align(Alignment.BottomEnd) // Posisi di kanan bawah
+                    .padding(24.dp) // Padding dari tepi layar
             ) {
-                Icon(imageVector = Icons.Default.Add, contentDescription = "Add Klien")
+                Icon(
+                    imageVector = Icons.Default.Add,
+                    contentDescription = "Add Klien",
+                    tint = Color.White // Warna ikon menjadi putih untuk kontras
+                )
             }
         }
-    ) { innerPadding ->
-        KlienStatus(
-            klienUiState = viewModel.klienUiState,
-            retryAction = { viewModel.getKlien() },
-            modifier = Modifier.padding(innerPadding),
-            onDetailClick = onDetailClick
-        )
     }
 }
 
@@ -109,7 +157,7 @@ fun KlienStatus(
             } else {
                 KlienLayout(
                     klien = klienUiState.klien,
-                    modifier = modifier.fillMaxWidth(),
+                    modifier = modifier,
                     onDetailClick = { onDetailClick(it.idKlien) },
                 )
             }
@@ -125,7 +173,7 @@ fun KlienLayout(
     onDetailClick: (Klien) -> Unit,
 ) {
     LazyColumn(
-        modifier = modifier,
+        modifier = Modifier.fillMaxHeight(), // Mengisi tinggi penuh
         contentPadding = PaddingValues(16.dp),
         verticalArrangement = Arrangement.spacedBy(16.dp)
     ) {
@@ -146,28 +194,90 @@ fun KlienCard(
     modifier: Modifier = Modifier,
 ) {
     Card(
-        modifier = modifier,
-        shape = MaterialTheme.shapes.medium,
-        elevation = CardDefaults.cardElevation(defaultElevation = 8.dp)
+        modifier = modifier
+            .padding(6.dp)
+            .shadow(8.dp, RoundedCornerShape(24.dp)) // Tambahkan bayangan
+            .clip(RoundedCornerShape(24.dp)),
+        colors = CardDefaults.cardColors(
+            containerColor = colorResource(R.color.primary) // Warna utama
+        ),
     ) {
-        Column(
-            modifier = Modifier.padding(16.dp),
-            verticalArrangement = Arrangement.spacedBy(8.dp)
+        Box(
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(120.dp)
         ) {
-            Row(
-                modifier = Modifier.fillMaxWidth(),
-                verticalAlignment = Alignment.CenterVertically
-            ) {
-                Text(
-                    text = klien.namaKlien,
-                    style = MaterialTheme.typography.titleLarge,
-                )
-                Spacer(Modifier.weight(1f))
-            }
+            // Latar belakang berupa dekorasi
+            Image(
+                painter = painterResource(id = R.drawable.logo), // Ganti dengan id gambar Anda
+                contentDescription = null,
+                modifier = Modifier.fillMaxSize(),
+                alpha = 0.5f // Transparansi
+            )
 
-            Text(
-                text = "Kontak: ${klien.kontakKlien}",
-                style = MaterialTheme.typography.bodyMedium
+            Column(
+                modifier = Modifier.padding(16.dp),
+                verticalArrangement = Arrangement.spacedBy(12.dp)
+            ) {
+                Row(
+                    modifier = Modifier.fillMaxWidth(),
+                    verticalAlignment = Alignment.CenterVertically
+                ) {
+                    Text(
+                        text = klien.namaKlien,
+                        style = MaterialTheme.typography.titleLarge.copy(fontWeight = FontWeight.Bold),
+                        color = Color.White
+                    )
+                    Spacer(Modifier.weight(1f))
+                    Row(
+                        verticalAlignment = Alignment.CenterVertically // Baris untuk tanggal
+                    ) {
+                        Icon(
+                            imageVector = Icons.Default.Menu, // Ikon tanggal
+                            contentDescription = "",
+                            tint = Color.White, // Warna emas
+                            modifier = Modifier.size(15.dp) // Ukuran ikon lebih kecil
+                        )
+                        Spacer(modifier = Modifier.width(5.dp)) // Jarak lebih kecil untuk elemen tanggal
+                        Text(
+                            text = klien.idKlien,
+                            style = MaterialTheme.typography.bodySmall, // Gaya teks lebih kecil
+                            color = Color.White
+                        )
+                    }
+                }
+
+                Divider(
+                    color = colorResource(R.color.yellow),
+                    thickness = 1.dp,
+                    modifier = Modifier.fillMaxWidth()
+                )
+
+                Row(
+                    verticalAlignment = Alignment.CenterVertically
+                ) {
+                    Icon(
+                        imageVector = Icons.Default.Call, // Ikon lokasi
+                        contentDescription = "",
+                        tint = Color.White,
+                        modifier = Modifier.size(20.dp)
+                    )
+                    Spacer(modifier = Modifier.width(8.dp))
+                    Text(
+                        text = "Kontak: ${klien.kontakKlien}",
+                        style = MaterialTheme.typography.bodySmall,
+                        color = Color.White.copy(alpha = 0.8f)
+                    )
+                }
+            }
+            Icon(
+                imageVector = Icons.Default.ArrowForward, // Ikon panah ke kanan
+                contentDescription = "",
+                tint = Color.White,
+                modifier = Modifier
+                    .align(Alignment.BottomEnd) // Posisikan di pojok kanan bawah
+                    .padding(15.dp) // Beri jarak dari tepi
+                    .size(25.dp) // Ukuran ikon
             )
         }
     }
