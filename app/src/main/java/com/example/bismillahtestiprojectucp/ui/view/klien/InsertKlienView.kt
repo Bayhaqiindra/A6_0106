4f6d907ffc05f72c2527abc558e60893555c8dbb
Bayhaqiindra <127074133+Bayhaqiindra@users.noreply.github.com>
2025-01-28 04:43:43 +0700

    Memperbaiki code Tampilan

--- app/src/main/java/com/example/bismillahtestiprojectucp/ui/view/klien/InsertKlienView.kt
+++ app/src/main/java/com/example/bismillahtestiprojectucp/ui/view/klien/InsertKlienView.kt
@@ -1,150 +1,231 @@
 package com.example.bismillahtestiprojectucp.ui.view.klien
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.OutlinedTextFieldDefaults
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.input.nestedscroll.nestedScroll
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.bismillahtestiprojectucp.R
 import com.example.bismillahtestiprojectucp.navigation.DestinasiNavigasi
-import com.example.bismillahtestiprojectucp.ui.customwidget.CostumeTopAppBar
-import com.example.bismillahtestiprojectucp.ui.view.acara.DestinasiAcaraInsert
-import com.example.bismillahtestiprojectucp.ui.viewmodel.acara.AcaraPenyediaViewModel
-import com.example.bismillahtestiprojectucp.ui.viewmodel.klien.HomeKlienViewModel
 import com.example.bismillahtestiprojectucp.ui.viewmodel.klien.InsertKlienUiEvent
 import com.example.bismillahtestiprojectucp.ui.viewmodel.klien.InsertKlienUiState
 import com.example.bismillahtestiprojectucp.ui.viewmodel.klien.InsertKlienViewModel
 import com.example.bismillahtestiprojectucp.ui.viewmodel.klien.KlienPenyediaViewModel
-import com.example.bismillahtestiprojectucp.ui.viewmodel.klien.KlienUiState
-import com.example.bismillahtestiprojectucp.ui.viewmodel.lokasi.HomeLokasiViewModel
-import com.example.bismillahtestiprojectucp.ui.viewmodel.lokasi.LokasiPenyediaViewModel
-import com.example.bismillahtestiprojectucp.ui.viewmodel.lokasi.LokasiUiState
-import com.example.bismillahtestiprojectucp.ui.widget.DynamicSelectedTextField
 import kotlinx.coroutines.launch
 
-    object DestinasiKlienInsert : DestinasiNavigasi {
-        override val route = "insert_klien"
-        override val titleRes = "Masukan Data Klien"
-    }
+// Definisi objek destinasi
+object DestinasiKlienInsert : DestinasiNavigasi {
+    override val route = "insert_klien"
+    override val titleRes = "Masukan Data Klien"
+}
 
-    @OptIn(ExperimentalMaterial3Api::class)
-    @Composable
-    fun InsertKlienScreen(
-        navigateBack: () -> Unit,
-        modifier: Modifier = Modifier,
-        viewModel: InsertKlienViewModel = viewModel(factory = KlienPenyediaViewModel.Factory),
+// Fungsi utama layar untuk memasukkan data klien
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun InsertKlienScreen(
+    navigateBack: () -> Unit,
+    modifier: Modifier = Modifier,
+    viewModel: InsertKlienViewModel = viewModel(factory = KlienPenyediaViewModel.Factory),
+) {
+    val coroutineScope = rememberCoroutineScope()
+
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(color = colorResource(R.color.primary))
     ) {
-        val coroutineScope = rememberCoroutineScope()
-        val scrollBehavior = TopAppBarDefaults.enterAlwaysScrollBehavior()
-
-        Scaffold(
-            modifier = modifier.nestedScroll(scrollBehavior.nestedScrollConnection),
-            topBar = {
-                CostumeTopAppBar(
-                    title = DestinasiKlienInsert.titleRes,
-                    canNavigateBack = true,
-                    scrollBehavior = scrollBehavior,
-                )
-            },
-        ) { innerPadding ->
-            EntryBodyKlien(
-                insertKlienUiState = viewModel.uiklienState,
-                onKlienValueChange = viewModel::updateInsertKlienState,
-                onSaveClick = {
-                    coroutineScope.launch {
-                        viewModel.insertKlien() // Menyimpan data pekerja
-                        navigateBack() // Navigasi kembali ke HomePekerja
-                    }
-                },
-                modifier = Modifier
-                    .padding(innerPadding)
-                    .verticalScroll(rememberScrollState())
-                    .fillMaxWidth()
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(start = 16.dp),
+            horizontalArrangement = Arrangement.SpaceBetween,
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Text(
+                text = "Masukan Data Klien",
+                fontFamily = FontFamily.Serif,
+                fontWeight = FontWeight.Bold,
+                style = TextStyle(
+                    fontSize = 25.sp
+                ),
+                color = Color.White,
+            )
+            Image(
+                painter = painterResource(id = R.drawable.logo),
+                contentDescription = "Logo",
+                modifier = Modifier.size(110.dp),
             )
         }
-    }
 
-    @Composable
-    fun EntryBodyKlien(
-        insertKlienUiState: InsertKlienUiState,
-        onKlienValueChange: (InsertKlienUiEvent) -> Unit,
-        onSaveClick: () -> Unit,
-        modifier: Modifier = Modifier
-    ) {
-        Column(
-            verticalArrangement = Arrangement.spacedBy(18.dp),
-            modifier = modifier.padding(12.dp)
+        Box(
+            modifier = Modifier
+                .fillMaxSize()
+                .clip(RoundedCornerShape(topStart = 30.dp, topEnd = 30.dp))
+                .background(color = colorResource(R.color.yellow))
+                .offset(y = -10.dp)
+                .padding(top = 8.dp, bottom = 40.dp)
         ) {
-            FormInputKlien(
-                insertKlienUiEvent = insertKlienUiState.insertUiEvent,
-                onValueChange = onKlienValueChange,
-                modifier = Modifier.fillMaxWidth(),
-            )
+            Column(
+                modifier = Modifier
+                    .padding(horizontal = 16.dp, vertical = 8.dp)
+                    .verticalScroll(rememberScrollState())
+            ) {
+                EntryBodyKlien(
+                    insertKlienUiState = viewModel.uiklienState,
+                    onKlienValueChange = viewModel::updateInsertKlienState,
+                    onSaveClick = {
+                        coroutineScope.launch {
+                            viewModel.insertKlien()
+                            navigateBack()
+                        }
+                    }
+                )
+            }
+        }
+    }
+}
+
+// Fungsi untuk menampilkan form input dan tombol simpan
+@Composable
+fun EntryBodyKlien(
+    insertKlienUiState: InsertKlienUiState,
+    onKlienValueChange: (InsertKlienUiEvent) -> Unit,
+    onSaveClick: () -> Unit,
+    modifier: Modifier = Modifier
+) {
+    Column(
+        verticalArrangement = Arrangement.spacedBy(18.dp),
+        modifier = modifier.padding(12.dp)
+    ) {
+        FormInputKlien(
+            insertKlienUiEvent = insertKlienUiState.insertUiEvent,
+            onValueChange = onKlienValueChange,
+            modifier = Modifier.fillMaxWidth(),
+        )
+        Row(
+            modifier = Modifier
+                .fillMaxWidth(),
+            horizontalArrangement = Arrangement.End
+        ) {
             Button(
                 onClick = onSaveClick,
-                shape = MaterialTheme.shapes.small,
-                modifier = Modifier.fillMaxWidth(),
-                enabled = insertKlienUiState.insertUiEvent.idKlien.isNotEmpty() // Validasi form
+                shape = RoundedCornerShape(16.dp),
+                modifier = Modifier
+                    .height(40.dp),
+                enabled = insertKlienUiState.insertUiEvent.idKlien.isNotEmpty(),
+                colors = ButtonDefaults.buttonColors(
+                    containerColor = colorResource(R.color.primary),
+                    contentColor = Color.White,
+                    disabledContainerColor = Color.Gray,
+                    disabledContentColor = Color.LightGray
+                )
             ) {
                 if (insertKlienUiState.isLoading) {
-                    CircularProgressIndicator(modifier = Modifier.size(24.dp)) // Loading indicator
+                    CircularProgressIndicator(
+                        modifier = Modifier.size(24.dp),
+                        color = Color.White
+                    )
                 } else {
                     Text(text = "Simpan")
                 }
             }
         }
     }
+}
 
-    @Composable
-    fun FormInputKlien(
-        insertKlienUiEvent: InsertKlienUiEvent,
-        modifier: Modifier = Modifier,
-        onValueChange: (InsertKlienUiEvent) -> Unit = {},
-        enabled: Boolean = true,
-    ) { // Mendapatkan list tanaman
-        Column(
-            modifier = modifier,
-            verticalArrangement = Arrangement.spacedBy(12.dp)
-        ) {
-            OutlinedTextField(
-                value = insertKlienUiEvent.idKlien,
-                onValueChange = { onValueChange(insertKlienUiEvent.copy(idKlien = it)) },
-                label = { Text("ID Klien") },
-                modifier = Modifier.fillMaxWidth(),
-                enabled = enabled,
-                singleLine = true
+// Fungsi untuk form input data klien
+@Composable
+fun FormInputKlien(
+    insertKlienUiEvent: InsertKlienUiEvent,
+    modifier: Modifier = Modifier,
+    onValueChange: (InsertKlienUiEvent) -> Unit = {},
+    enabled: Boolean = true,
+) {
+    Column(
+        modifier = modifier,
+        verticalArrangement = Arrangement.spacedBy(12.dp)
+    ) {
+        OutlinedTextField(
+            value = insertKlienUiEvent.idKlien,
+            onValueChange = { onValueChange(insertKlienUiEvent.copy(idKlien = it)) },
+            label = { Text("ID Klien", color = Color.White) },
+            modifier = Modifier.fillMaxWidth(),
+            enabled = enabled,
+            singleLine = true,
+            shape = RoundedCornerShape(16.dp),
+            colors = OutlinedTextFieldDefaults.colors(
+                unfocusedTextColor = Color.White,
+                focusedTextColor = Color.White,
+                focusedBorderColor = Color.White,
+                unfocusedBorderColor = Color.White
             )
+        )
 
-            OutlinedTextField(
-                value = insertKlienUiEvent.namaKlien,
-                onValueChange = { onValueChange(insertKlienUiEvent.copy(namaKlien = it)) },
-                label = { Text("Nama Klien") },
-                modifier = Modifier.fillMaxWidth(),
-                enabled = enabled,
-                singleLine = true
+        OutlinedTextField(
+            value = insertKlienUiEvent.namaKlien,
+            onValueChange = { onValueChange(insertKlienUiEvent.copy(namaKlien = it)) },
+            label = { Text("Nama Klien", color = Color.White) },
+            modifier = Modifier.fillMaxWidth(),
+            enabled = enabled,
+            singleLine = true,
+            shape = RoundedCornerShape(16.dp),
+            colors = OutlinedTextFieldDefaults.colors(
+                unfocusedTextColor = Color.White,
+                focusedTextColor = Color.White,
+                focusedBorderColor = Color.White,
+                unfocusedBorderColor = Color.White
             )
-            OutlinedTextField(
-                value = insertKlienUiEvent.kontakKlien,
-                onValueChange = { onValueChange(insertKlienUiEvent.copy(kontakKlien = it)) },
-                label = { Text("Kontak Klien") },
-                modifier = Modifier.fillMaxWidth(),
-                enabled = enabled,
-                singleLine = true
+        )
+
+        OutlinedTextField(
+            value = insertKlienUiEvent.kontakKlien,
+            onValueChange = { onValueChange(insertKlienUiEvent.copy(kontakKlien = it)) },
+            label = { Text("Kontak Klien", color = Color.White) },
+            modifier = Modifier.fillMaxWidth(),
+            enabled = enabled,
+            singleLine = true,
+            shape = RoundedCornerShape(16.dp),
+            colors = OutlinedTextFieldDefaults.colors(
+                unfocusedTextColor = Color.White,
+                focusedTextColor = Color.White,
+                focusedBorderColor = Color.White,
+                unfocusedBorderColor = Color.White
             )
-        }
+        )
     }
+}
