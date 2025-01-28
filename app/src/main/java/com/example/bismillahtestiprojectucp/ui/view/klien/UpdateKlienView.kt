24681aba354179bf9c21a12b81a00183d297efad
Bayhaqiindra <127074133+Bayhaqiindra@users.noreply.github.com>
2025-01-28 22:32:42 +0700

    Memperbaiki code tampilan

--- app/src/main/java/com/example/bismillahtestiprojectucp/ui/view/klien/UpdateKlienView.kt
+++ app/src/main/java/com/example/bismillahtestiprojectucp/ui/view/klien/UpdateKlienView.kt
@@ -1,17 +1,39 @@
 package com.example.bismillahtestiprojectucp.ui.view.klien
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.input.nestedscroll.nestedScroll
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.bismillahtestiprojectucp.R
 import com.example.bismillahtestiprojectucp.navigation.DestinasiNavigasi
 import com.example.bismillahtestiprojectucp.ui.viewmodel.klien.KlienPenyediaViewModel
 import com.example.bismillahtestiprojectucp.ui.viewmodel.klien.UpdateKlienViewModel
@@ -28,38 +50,66 @@ object DestinasiUpdateKlien : DestinasiNavigasi {
 @Composable
 fun UpdateKlienView(
     navigateBack: () -> Unit,
-    onNavigateUp: () -> Unit,// Navigasi kembali ke HomePekerja
+    onNavigateUp: () -> Unit, // Navigasi kembali ke HomePekerja
     modifier: Modifier = Modifier,
     viewModel: UpdateKlienViewModel = viewModel(factory = KlienPenyediaViewModel.Factory),
 ) {
 
-
     val coroutineScope = rememberCoroutineScope()
-    val scrollBehavior = TopAppBarDefaults.enterAlwaysScrollBehavior()
 
-    Scaffold(
-        modifier = modifier.nestedScroll(scrollBehavior.nestedScrollConnection),
-//        topBar = {
-//            CostumeTopAppBar(
-//                title = DestinasiUpdateKlien.titleRes,
-//                canNavigateBack = true,
-//                navigateUp = onNavigateUp
-//            )
-//        }
-    ) { innerPadding ->
-        EntryBodyKlien(
-            insertKlienUiState = viewModel.uiklienState,
-            onKlienValueChange = viewModel::updateInsertKlienState,
-            onSaveClick = {
-                coroutineScope.launch {
-                    viewModel.updateKlien() // Menyimpan data pekerja
-                    navigateBack() // Navigasi kembali ke HomePekerja
-                }
-            },
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(color = colorResource(R.color.primary))
+    ) {
+        // Header bagian atas
+        Row(
             modifier = Modifier
-                .padding(innerPadding)
-                .verticalScroll(rememberScrollState())
                 .fillMaxWidth()
-        )
+                .padding(start = 16.dp, top = 16.dp, end = 16.dp),
+            horizontalArrangement = Arrangement.SpaceBetween,
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Text(
+                text = "Update Data Klien",
+                fontFamily = FontFamily.Serif,
+                fontWeight = FontWeight.Bold,
+                style = TextStyle(fontSize = 25.sp),
+                color = Color.White,
+            )
+            Image(
+                painter = painterResource(id = R.drawable.logo),
+                contentDescription = "Logo",
+                modifier = Modifier.size(80.dp), // Ukuran logo disesuaikan agar proporsional
+            )
+        }
+
+        // Konten bagian bawah
+        Box(
+            modifier = Modifier
+                .fillMaxSize()
+                .clip(RoundedCornerShape(topStart = 30.dp, topEnd = 30.dp))
+                .background(color = colorResource(R.color.yellow))
+                .offset(y = (-10).dp)
+        ) {
+            Column(
+                modifier = Modifier
+                    .padding(horizontal = 16.dp, vertical = 8.dp)
+                    .verticalScroll(rememberScrollState())
+            ) {
+                EntryBodyKlien(
+                    insertKlienUiState = viewModel.uiklienState,
+                    onKlienValueChange = viewModel::updateInsertKlienState,
+                    onSaveClick = {
+                        coroutineScope.launch {
+                            viewModel.updateKlien() // Menyimpan data pekerja
+                            navigateBack() // Navigasi kembali ke HomePekerja
+                        }
+                    },
+                    modifier = Modifier
+                        .fillMaxWidth()
+                )
+            }
+        }
     }
 }
