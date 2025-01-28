fbadf0afb0fc08443244e6ccb65899586be9ff03
Bayhaqiindra <127074133+Bayhaqiindra@users.noreply.github.com>
2025-01-28 22:35:03 +0700

    Memperbaiki Code Tampilan

--- app/src/main/java/com/example/bismillahtestiprojectucp/ui/view/lokasi/DetailLokasiView.kt
+++ app/src/main/java/com/example/bismillahtestiprojectucp/ui/view/lokasi/DetailLokasiView.kt
@@ -1,18 +1,24 @@
 package com.example.bismillahtestiprojectucp.ui.view.lokasi
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Edit
 import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.CircularProgressIndicator
@@ -32,13 +38,20 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.input.nestedscroll.nestedScroll
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.bismillahtestiprojectucp.R
 import com.example.bismillahtestiprojectucp.model.Lokasi
 import com.example.bismillahtestiprojectucp.navigation.DestinasiNavigasi
-import com.example.bismillahtestiprojectucp.ui.customwidget.CostumeTopAppBar
 import com.example.bismillahtestiprojectucp.ui.viewmodel.lokasi.DetailLokasiUiState
 import com.example.bismillahtestiprojectucp.ui.viewmodel.lokasi.DetailLokasiViewModel
 import com.example.bismillahtestiprojectucp.ui.viewmodel.lokasi.LokasiPenyediaViewModel
@@ -58,47 +71,79 @@ fun DetailLokasiView(
     onEditClick: (String) -> Unit,
     detailLokasiViewModel: DetailLokasiViewModel = viewModel(factory = LokasiPenyediaViewModel.Factory)
 ) {
-    val scrollBehavior = TopAppBarDefaults.enterAlwaysScrollBehavior()
-
     LaunchedEffect(Unit) {
         detailLokasiViewModel.getLokasiById()
     }
 
-    Scaffold(
-        modifier = modifier.nestedScroll(scrollBehavior.nestedScrollConnection),
-        topBar = {
-            CostumeTopAppBar(
-                title = DestinasiDetailLokasi.titleRes,
-                canNavigateBack = true,
-                scrollBehavior = scrollBehavior,
-                navigateUp = navigateBack
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(color = colorResource(R.color.primary)),
+    ) {
+
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(start = 16.dp),
+            horizontalArrangement = Arrangement.SpaceBetween,
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Text(
+                text = "Detail Lokasi",
+                fontFamily = FontFamily.Serif,
+                fontWeight = FontWeight.Bold,
+                style = TextStyle(
+                    fontSize = 35.sp
+                ),
+                color = Color.White,
             )
-        },
-        floatingActionButton = {
+            Image(
+                painter = painterResource(id = R.drawable.logo),
+                contentDescription = "Logo",
+                modifier = Modifier
+                    .size(110.dp),
+            )
+        }
+
+        Box(
+            modifier = Modifier
+                .fillMaxSize()
+                .clip(RoundedCornerShape(topStart = 30.dp, topEnd = 30.dp))
+                .background(color = colorResource(R.color.yellow))
+                .offset(y = -10.dp)
+                .padding(top = 8.dp, bottom = 40.dp)
+        ) {
+            Column(
+                modifier = Modifier.padding(16.dp)
+            ) {
+                DetailLokasiContent(
+                    detailLokasiUiState = detailLokasiViewModel.detailLokasiUiState,
+                    modifier = Modifier.fillMaxWidth(),
+                    onDeleteClick = {
+                        detailLokasiViewModel.deleteLokasi()
+                        navigateBack()
+                    }
+                )
+            }
+
             FloatingActionButton(
                 onClick = {
                     val idLokasi = (detailLokasiViewModel.detailLokasiUiState as? DetailLokasiUiState.Success)?.lokasi?.idLokasi
                     if (idLokasi != null) onEditClick(idLokasi)
                 },
-                shape = MaterialTheme.shapes.medium
+                shape = RoundedCornerShape(20.dp),
+                containerColor = colorResource(R.color.primary),
+                modifier = Modifier
+                    .align(Alignment.BottomEnd)
+                    .padding(24.dp)
             ) {
                 Icon(
                     imageVector = Icons.Default.Edit,
-                    contentDescription = "Edit Lokasi"
+                    contentDescription = "Edit Lokasi",
+                    tint = Color.White
                 )
             }
         }
-    ) { innerPadding ->
-        DetailLokasiContent(
-            detailLokasiUiState = detailLokasiViewModel.detailLokasiUiState,
-            modifier = Modifier
-                .padding(innerPadding)
-                .fillMaxSize(),
-            onDeleteClick = {
-                detailLokasiViewModel.deleteLokasi()
-                navigateBack()
-            }
-        )
     }
 }
 
@@ -132,9 +177,14 @@ fun DetailLokasiContent(
                 DetailLokasiCard(lokasi = detailLokasiUiState.lokasi)
                 Button(
                     onClick = { deleteConfirmationRequired = true },
-                    modifier = Modifier.fillMaxWidth()
+                    shape = RoundedCornerShape(16.dp),
+                    modifier = Modifier.height(40.dp),
+                    colors = ButtonDefaults.buttonColors(
+                        containerColor = colorResource(R.color.primary),
+                        contentColor = Color.White,
+                    )
                 ) {
-                    Text("Hapus Lokasi")
+                    Text("Delete")
                 }
             }
         }
@@ -159,7 +209,10 @@ fun DetailLokasiCard(
     Card(
         modifier = modifier.fillMaxWidth(),
         elevation = CardDefaults.cardElevation(defaultElevation = 8.dp),
-        shape = RoundedCornerShape(12.dp)
+        shape = RoundedCornerShape(12.dp),
+        colors = CardDefaults.cardColors(
+            containerColor = colorResource(R.color.primary)
+        )
     ) {
         Column(
             modifier = Modifier.padding(16.dp),
@@ -187,13 +240,13 @@ fun ComponentDetailLokasi(
             text = judul,
             style = MaterialTheme.typography.titleMedium.copy(
                 fontWeight = FontWeight.Bold,
-                color = MaterialTheme.colorScheme.primary
+                color = colorResource(R.color.yellow)
             )
         )
         Text(
             text = isinya,
             style = MaterialTheme.typography.bodyLarge.copy(
-                color = MaterialTheme.colorScheme.onSurface
+                color = Color.White
             )
         )
     }
