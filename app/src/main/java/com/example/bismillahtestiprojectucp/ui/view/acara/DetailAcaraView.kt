e5c8b5f3f6642704cbcaa484e7c86126bf0392a6
Bayhaqiindra <127074133+Bayhaqiindra@users.noreply.github.com>
2025-01-28 22:35:34 +0700

    Memperbaiki Code Tampilan

--- app/src/main/java/com/example/bismillahtestiprojectucp/ui/view/acara/DetailAcaraView.kt
+++ app/src/main/java/com/example/bismillahtestiprojectucp/ui/view/acara/DetailAcaraView.kt
@@ -1,18 +1,24 @@
 package com.example.bismillahtestiprojectucp.ui.view.acara
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Edit
 import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.CircularProgressIndicator
@@ -32,14 +38,20 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.input.nestedscroll.nestedScroll
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.bismillahtestiprojectucp.R
 import com.example.bismillahtestiprojectucp.model.Acara
 import com.example.bismillahtestiprojectucp.navigation.DestinasiNavigasi
-import com.example.bismillahtestiprojectucp.ui.customwidget.CostumeTopAppBar
-import com.example.bismillahtestiprojectucp.ui.view.acara.DestinasiDetailAcara.idAcara
 import com.example.bismillahtestiprojectucp.ui.viewmodel.acara.AcaraPenyediaViewModel
 import com.example.bismillahtestiprojectucp.ui.viewmodel.acara.DetailAcaraUiState
 import com.example.bismillahtestiprojectucp.ui.viewmodel.acara.DetailAcaraViewModel
@@ -59,49 +71,76 @@ fun DetailAcaraView(
     onEditClick: (String) -> Unit,
     detailAcaraViewModel: DetailAcaraViewModel = viewModel(factory = AcaraPenyediaViewModel.Factory),
 ) {
-    val scrollBehavior = TopAppBarDefaults.enterAlwaysScrollBehavior()
-
     LaunchedEffect(Unit) {
         detailAcaraViewModel.getAcaraById()
     }
 
-    Scaffold(
-        modifier = modifier.nestedScroll(scrollBehavior.nestedScrollConnection),
-        topBar = {
-            CostumeTopAppBar(
-                title = DestinasiDetailAcara.titleRes,
-                canNavigateBack = true,
-                scrollBehavior = scrollBehavior,
-                navigateUp = navigateBack
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(color = colorResource(R.color.primary)),
+    ) {
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(start = 16.dp),
+            horizontalArrangement = Arrangement.SpaceBetween,
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Text(
+                text = "Detail Acara",
+                fontFamily = FontFamily.Serif,
+                fontWeight = FontWeight.Bold,
+                style = TextStyle(fontSize = 35.sp),
+                color = Color.White,
             )
-        },
-        floatingActionButton = {
+            Image(
+                painter = painterResource(id = R.drawable.logo),
+                contentDescription = "Logo",
+                modifier = Modifier.size(110.dp),
+            )
+        }
+
+        Box(
+            modifier = Modifier
+                .fillMaxSize()
+                .clip(RoundedCornerShape(topStart = 30.dp, topEnd = 30.dp))
+                .background(color = colorResource(R.color.yellow))
+                .offset(y = -10.dp)
+                .padding(top = 8.dp, bottom = 40.dp)
+        ) {
+            Column(modifier = Modifier.padding(16.dp)) {
+                DetailAcaraContent(
+                    dtlAcaraUiState = detailAcaraViewModel.detailAcaraUiState,
+                    modifier = Modifier.fillMaxWidth(),
+                    onDeleteClick = {
+                        detailAcaraViewModel.deleteAcara()
+                        navigateBack()
+                    }
+                )
+            }
+
             FloatingActionButton(
                 onClick = {
                     val idAcara = (detailAcaraViewModel.detailAcaraUiState as? DetailAcaraUiState.Success)?.acara?.idAcara
-                    if (idAcara != null) onEditClick(idAcara.toString())
+                    if (idAcara != null) onEditClick(idAcara)
                 },
-                shape = MaterialTheme.shapes.medium
+                shape = RoundedCornerShape(20.dp),
+                containerColor = colorResource(R.color.primary),
+                modifier = Modifier
+                    .align(Alignment.BottomEnd)
+                    .padding(24.dp)
             ) {
                 Icon(
                     imageVector = Icons.Default.Edit,
-                    contentDescription = "Edit Acara"
+                    contentDescription = "Edit Acara",
+                    tint = Color.White
                 )
             }
         }
-    ) { innerPadding ->
-        DetailAcaraContent(
-            dtlAcaraUiState = detailAcaraViewModel.detailAcaraUiState,
-            modifier = Modifier
-                .padding(innerPadding)
-                .fillMaxSize(),
-            onDeleteClick = {
-                detailAcaraViewModel.deleteAcara()
-                navigateBack()
-            }
-        )
     }
 }
+
 
 @Composable
 fun DetailAcaraContent(
@@ -133,9 +172,14 @@ fun DetailAcaraContent(
                 DetailAcaraCard(acara = dtlAcaraUiState.acara)
                 Button(
                     onClick = { deleteConfirmationRequired = true },
-                    modifier = Modifier.fillMaxWidth()
+                    shape = RoundedCornerShape(16.dp),
+                    modifier = Modifier.height(40.dp),
+                    colors = ButtonDefaults.buttonColors(
+                        containerColor = colorResource(R.color.primary),
+                        contentColor = Color.White,
+                    )
                 ) {
-                    Text("Hapus Acara")
+                    Text("Delete")
                 }
             }
         }
@@ -160,7 +204,9 @@ fun DetailAcaraCard(
     Card(
         modifier = modifier.fillMaxWidth(),
         elevation = CardDefaults.cardElevation(defaultElevation = 8.dp),
-        shape = RoundedCornerShape(12.dp)
+        colors = CardDefaults.cardColors(
+            containerColor = colorResource(R.color.primary)
+        )
     ) {
         Column(
             modifier = Modifier.padding(16.dp),
@@ -191,13 +237,13 @@ fun ComponentDetailAcara(
             text = judul,
             style = MaterialTheme.typography.titleMedium.copy(
                 fontWeight = FontWeight.Bold,
-                color = MaterialTheme.colorScheme.primary
+                color = colorResource(R.color.yellow)
             )
         )
         Text(
             text = isinya,
             style = MaterialTheme.typography.bodyLarge.copy(
-                color = MaterialTheme.colorScheme.onSurface
+                color = Color.White
             )
         )
     }
